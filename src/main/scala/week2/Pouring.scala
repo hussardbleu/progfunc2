83d0f2f9ab2e2d2c24c0ad3054bd4cb1ddcf37d0
hussard bleu <bleuhussard@gmail.com>
2016-10-19 13:31:05 +0200

    Water Pouring Problem : defining class Pouring representing the problem : a vector of glasses with  given capacities,  an initial state for glasses, 3 allowed move (empty or fill a glass, pour one glass into another)

---
+++ src/main/scala/week2/Pouring.scala
@@ -0,0 +1,40 @@
+package week2
+
+
+class Pouring(capacity: Vector[Int]) {
+
+  //States
+
+  type State = Vector[Int]
+  val initialState = capacity map (x => 0)
+
+  // Moves
+
+  trait Move {
+    def change(state: State): State
+  }
+
+  case class Empty(glass: Int) extends Move {
+    def change(state: State): State = state updated(glass, 0)
+  }
+
+  case class Fill(glass: Int) extends Move {
+    def change(state: State): State = state updated(glass, capacity(glass))
+  }
+
+  case class Pour(from: Int, to: Int) extends Move {
+
+    def change(state: State): State = {
+      val amount = state(from) min  (capacity(to) - state(to))
+      state updated(to, state(to) + amount) updated (from, state(from) - amount)
+    }
+  }
+
+
+  val glasses = 0 until capacity.length
+
+  val moves =
+    (for (g <- glasses) yield Empty(g)) ++
+    (for (g <- glasses) yield Fill(g)) ++
+    (for (from <- glasses; to <- glasses if from != to) yield Pour(from, to))
+}
---
+++ src/main/scala/week2/water.sc
@@ -0,0 +1,7 @@
+import week2.Pouring
+
+val problem = new Pouring(Vector(4, 7))
+
+problem.moves
+
+
