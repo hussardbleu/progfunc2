e0a0a347faeed31299871a0b246284d91e6edd75
hussard bleu <bleuhussard@gmail.com>
2016-10-20 09:45:51 +0200

    Water Pouring Problem : re-implement endState method using foldRight starting from initialState, applying change method from the end of historical list of Moves to the last move

--- src/main/scala/week2/Pouring.scala
+++ src/main/scala/week2/Pouring.scala
@@ -41,12 +41,6 @@ class Pouring(capacity: Vector[Int]) {
   // Paths
 
   class Path(history: List[Move]) {
-    def endState: State = trackState(history)
-    private def trackState(xs: List[Move]): State = xs match {
-      case Nil => initialState
-      case move :: xs1 => move change trackState(xs1)
-    }
-
-
+    def endState: State = (history foldRight initialState) ((x,y) => x change y)
   }
 }
